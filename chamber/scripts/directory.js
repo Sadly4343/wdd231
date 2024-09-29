dddccf50b5868205ec303a74094f98a782583a2a
Sadly4343 <loganhartshorn@gmail.com>
2024-09-28 20:15:49 -0600

    commit

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,17 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date()
+
+currentyear.innerHTML = `@ <span
+class="highlight">${today.getFullYear()}</span>`;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = date;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
+
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
