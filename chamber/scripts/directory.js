8a058ca36f5597a1a92a311bbd43b2d701e88696
Sadly4343 <loganhartshorn@gmail.com>
2024-10-02 00:00:20 -0600

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -15,3 +15,39 @@ hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+
+const url = 'https://raw.githubusercontent.com/Sadly4343/wdd231/refs/heads/main/chamber/data/members.json'
+const cards = document.querySelector('#cards')
+
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.companies); // temporary testing of data response
+    displayCompanies(data.companies);
+}
+
+getCompanyData();
+
+const displayCompanies = (companies) => {
+    companies.forEach((companie) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h8'); // fill in the blank
+        let portrait = document.createElement('img');
+
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${companie.name} totally`; // fill in the blank
+        // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute('src', companie.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${companie.name} totally`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName); //fill in the blank
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+}
