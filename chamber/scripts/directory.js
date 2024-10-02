ba5302cca5d97e5014033f53e50c028ab9d18b6b
Sadly4343 <loganhartshorn@gmail.com>
2024-10-02 00:16:05 -0600

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -33,20 +33,21 @@ const displayCompanies = (companies) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let fullName = document.createElement('h8'); // fill in the blank
-        let portrait = document.createElement('img');
+        let image = document.createElement('img');
 
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${companie.name} totally`; // fill in the blank
-        // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', companie.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${companie.name} totally`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        // Build the image images by setting all the relevant attributes
+        console.log(`Image URL: ${companie.image}`);
+        image.setAttribute('src', companie.image);
+        image.setAttribute('alt', `images of ${companie.name} totally`); // fill in the blank
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '340');
+        image.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
-        card.appendChild(portrait);
+        card.appendChild(image);
 
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
