c565e9b0c21f2d66e87fbab7d5ffffd46333161c
Sadly4343 <loganhartshorn@gmail.com>
2024-10-14 19:51:40 -0600

    commit

--- chamber/scripts/thank.js
+++ chamber/scripts/thank.js
@@ -5,4 +5,19 @@ console.log(currentUrl)
 const everything = currentUrl.split('?')
 console.log(everything)
 
+let formData = everything[1].split('&')
+console.log(formData)
+
+function show(end) {
+    console.log(end)
+    formData.forEach(element) => {
+        console.log(element)
+        if (element.startsWith(end)) {
+            result = element
+        }
+    }
+    return (result)
+}
+const showInfo = document.querySelector('#info')
+showInfo.innerHTML = show("phone")
 
