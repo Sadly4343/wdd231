e793bb87438f5dd21aa5f5e0102477b853dd2ff3
Sadly4343 <loganhartshorn@gmail.com>
2024-10-14 19:30:13 -0600

    commit

--- chamber/scripts/thank.js
+++ chamber/scripts/thank.js
@@ -1,6 +1,6 @@
-let numVisits = Number(window.localStorage.getItem("visits-1s"));
 
-
-let visitTag = document.getElementById('visits');
 const currentUrl = window.location.href;
 console.log(currentUrl)
+
+const everything = currentUrl.split('?')
+console.log(everything)
