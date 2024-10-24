dc4b95febdb841623f9f015e49623b6cd9294d2b
Sadly4343 <loganhartshorn@gmail.com>
2024-10-23 23:16:32 -0600

    commit

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,7 +1,11 @@
 
+const myKey = "uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg";
+const myURL = 
 
-(async () => {
-    const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[id]=263319';
+
+
+async function apiFetch() {
+    const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg';
     //    const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[common_name]=beach%20strawberry';
 
 
@@ -9,12 +13,12 @@
     const json = await response.json();
     //const data = JSON.parse(json.contents); // Parse the contents of the response
     console.log(json);
-})();
+}
+apiFetch();
 
-function searchPlantsByName() {
-    async () => {
-        const targetUrl = 'https://trefle.io/api/v1/plants/search?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&q=coconut';
-        //    const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[common_name]=beach%20strawberry';
+async function searchPlantsByName() {
+
+        const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[common_name]=beach%20strawberry';
 
 
         const response = await fetch(targetUrl);
@@ -22,24 +26,25 @@ function searchPlantsByName() {
         //const data = JSON.parse(json.contents); // Parse the contents of the response
         console.log(json);
     }
-}
-searchPlantsByName(name);
-function searchPlantsByFilter(filter) { }
+searchPlantsByName();
 
-function invokeServer(url) {
-    async () => {
-        const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[id]=263319';
+
+//function searchPlantsByFilter(filter) { }
+
+//function invokeServer(url) {
+    //async () => {
+        //const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[id]=263319';
         //    const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[common_name]=beach%20strawberry';
 
 
-        const response = await fetch(targetUrl);
-        const json = await response.json();
+        //const response = await fetch(targetUrl);
+        //const json = await response.json();
         //const data = JSON.parse(json.contents); // Parse the contents of the response
-        console.log(json);
-    }
-}
+        //console.log(json);
+    
 
-function getPlantById(id) { }
+
+//function getPlantById(id) { }
 /*
 async () => {
     const targetUrl = 'https://trefle.io/api/v1/plants?token=uPa9xse-P3R35sGhnwy-H0MQr0J6IJQHIyO2ZG7OZeg&filter[id]=263319';
