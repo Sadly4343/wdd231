f49b362521064d0b5a2b0b03c80813df638bdcc6
Sadly4343 <loganhartshorn@gmail.com>
2024-10-25 20:48:53 -0600

    commit

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -8,6 +8,70 @@ function getDate() {
 
 
 }
+const urls = 'https://raw.githubusercontent.com/Sadly4343/wdd231/refs/heads/main/chamber/data/members.json'
+const cards = document.querySelector('#cards')
+companiesData = [];
+async function getCompanyData() {
+    const response = await fetch(urls);
+    const data = await response.json();
+    companiesData = data.companies;
+    shuffle(companiesData);
+    displayCompanies(data.companies);
+}
+
+getCompanyData();
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+    }
+}
+
+const displayCompanies = (companiesData) => {
+    let cardCount = 0;
+    companiesData.forEach((companie) => {
+
+        if (companie.membership != "1" && cardCount < 3) {
+            cardCount += 1
+
+            let card = document.createElement('section');
+            let fullName = document.createElement('h8');
+            let image = document.createElement('img');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
+            let website = document.createElement('a');
+            let membership = document.createElement('p');
+
+
+            fullName.textContent = companie.name;
+            address.textContent = companie.address;
+            phone.textContent = companie.phone;
+            website.textContent = companie.website;
+            website.textContent = companie.membership
+            website.setAttribute('href', companie.website)
+            image.setAttribute('src', companie.image);
+            image.setAttribute('alt', `images of ${companie.name} totally`);
+            image.setAttribute('loading', 'lazy');
+            image.setAttribute('width', '48');
+            image.setAttribute('height', '48');
+
+
+            card.appendChild(fullName);
+            card.appendChild(image);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(website);
+            card.appendChild(membership);
+
+            cards.appendChild(card);
+        }
+    });
+}
 
 document.querySelector('form').addEventListener('submit', getDate);
 
@@ -37,4 +101,20 @@ callAction.addEventListener("click", () => {
 })
 closeAction.addEventListener("click", () => {
     memberForm.close();
+})
+
+const adviceBtn = document.getElementById('advice');
+
+const libraryBtn = document.getElementById('library');
+
+const guideBtn = document.getElementById('guide');
+
+adviceBtn.addEventListener("click", () => {
+    window.location.href = 'care.html';
+})
+libraryBtn.addEventListener("click", () => {
+    window.location.href = 'library.html';
+})
+guideBtn.addEventListener("click", () => {
+    window.location.href = 'care.html';
 })
